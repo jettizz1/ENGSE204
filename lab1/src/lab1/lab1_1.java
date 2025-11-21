cc9c9d2c79c98ba36bc082f8c5bf2652a76b93b0
User <User@TG-202308241520>
2025-11-21 14:03:09 +0700

    summitlab

---
+++ lab1/src/lab1/lab1_1.java
@@ -0,0 +1,16 @@
+package lab1;
+import java.util.Scanner;
+public class lab1_1 {
+	public static void main(String[] args) {
+        int number1;
+        int number2;
+        Scanner input = new Scanner(System.in);
+        number1 = input.nextInt();
+        number2 = input.nextInt();
+        System.out.println(number1 + number2);
+        input.close();
+    }
+
+
+}
+
