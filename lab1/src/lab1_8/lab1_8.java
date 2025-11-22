2f3d92fa1abe9b5695812882a2db621025839f6f
User <User@TG-202308241520>
2025-11-22 16:30:34 +0700

    summitlab

--- lab1/src/lab1_8/lab1_8.java
+++ lab1/src/lab1_8/lab1_8.java
@@ -8,14 +8,14 @@ public class lab1_8 {
 		for (int i = 0; i < allarray.length; i++) {
 			int arraynum = input.nextInt();
 			allarray[i] = arraynum;
-		}//End For
+		}//End For Loop
 		
 		int minlove = allarray[0];
 		for (int i = 1; i < allarray.length; i++) {
 			if (allarray[i] > minlove) {
 				minlove = allarray[i];
 			}//End If
-		}//End For
+		}//End For Loop
 		System.out.println(minlove);
 		input.close();
 	}//End Main
