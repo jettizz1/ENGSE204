521f48a36dd3ebdc377c91a4909d80473341569e
User <User@TG-202308241520>
2025-11-22 16:28:03 +0700

    summitlab

---
+++ lab1/src/lab1_8/lab1_8.java
@@ -0,0 +1,22 @@
+package lab1_8;
+import java.util.Scanner;
+public class lab1_8 {
+	public static void main(String[]args) {
+		Scanner input = new Scanner(System.in);
+		int arrayone = input.nextInt();
+		int [] allarray = new int [arrayone];
+		for (int i = 0; i < allarray.length; i++) {
+			int arraynum = input.nextInt();
+			allarray[i] = arraynum;
+		}//End For
+		
+		int minlove = allarray[0];
+		for (int i = 1; i < allarray.length; i++) {
+			if (allarray[i] > minlove) {
+				minlove = allarray[i];
+			}//End If
+		}//End For
+		System.out.println(minlove);
+		input.close();
+	}//End Main
+}
