666d9904cc5e3a59ff3e0dc2c97981214f9fe49e
User <User@TG-202308241520>
2025-11-23 15:02:48 +0700

    summitlab

---
+++ lab1/src/lab1_14/lab1_14.java
@@ -0,0 +1,41 @@
+package lab1_14;
+import java.util.Scanner;
+public class lab1_14 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R = sc.nextInt();
+		int C = sc.nextInt();
+		sc.nextLine();
+		char [][] map = new char[R][C];
+		for (int i = 0; i < R; i++) {
+			String line = sc.nextLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = line.charAt(j);
+			}
+		}
+		int targetR = sc.nextInt();
+		int targetC = sc.nextInt();
+		if (map[targetR][targetC]== '*') {
+			System.out.println("Mine");
+			sc.close();
+			return;
+		}
+		int count = 0;
+		int[] dR = {-1, -1, -1, 0, 0, 1, 1, 1};
+		int[] dC = {-1, 0, 1, -1, 1, -1, 0, 1};
+		for (int k = 0; k < 8; k++) {
+			int nr = targetR + dR[k];
+			int nc = targetC + dC[k];
+			if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
+				if (map[nr][nc] == '*') {
+					count++;
+				}
+			}
+		}
+
+		System.out.println();
+		sc.close();
+	}
+}
+
+		
