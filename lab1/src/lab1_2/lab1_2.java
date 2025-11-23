d8737ddc00a74c9394b56adbaf2a109d94c3a585
User <User@TG-202308241520>
2025-11-23 16:06:26 +0700

    summitlab

--- lab1/src/lab1_2/lab1_2.java
+++ lab1/src/lab1_2/lab1_2.java
@@ -1,14 +1,11 @@
 package lab1_2;
 import java.util.Scanner;
-
 public class lab1_2 {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        float number1 = input.nextFloat();
-        float number2 = input.nextFloat();
-        System.out.println(number1 * number2);
-        input.close();
-    }
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		float numberone = input.nextFloat();
+		float numbertwoo = input.nextFloat();
+		System.out.println( numberone * numbertwoo );
+		input.close();
+	}//END MAIN
 }
