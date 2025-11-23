ba11259e17cca93d71904d5711a4adb77c6009da
User <User@TG-202308241520>
2025-11-23 16:16:01 +0700

    summitlab

--- lab1/src/lab1_3/lab1_3.java
+++ lab1/src/lab1_3/lab1_3.java
@@ -1,21 +1,14 @@
 package lab1_3;
 import java.util.Scanner;
-
 public class lab1_3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // รับค่าจำนวนเต็มจากผู้ใช้
-        System.out.print("Enter an integer: ");
-        int number = sc.nextInt();
-
-        // ตรวจสอบว่าเป็นเลขคู่หรือเลขคี่
-        if (number % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
-        }
-
-        sc.close();
-    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int numberuser = input.nextInt();
+		if ( numberuser % 2 == 0 ) {
+			System.out.println("Even");
+		}else {
+			System.out.println("Odd");
+		}//END IF
+		input.close();
+	}//END MAIN
 }
