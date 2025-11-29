60a62b7552cb83837ad4b1c72fb8e1dd91c1ae35
User <User@TG-202308241520>
2025-11-29 15:00:30 +0700

    summitlab

---
+++ lab2/src/lab2_2/lab2_2.java
@@ -0,0 +1,28 @@
+package lab2_2;
+import java.util.Scanner;
+public class lab2_2 {
+	public static void main(String[] args) {
+		class Studentuser {
+			String  studentId;
+			String name;
+			
+			void displayInfo() {
+				System.out.println(studentId);
+				System.out.println(name);
+				
+		}
+		
+}
+			Scanner sk = new Scanner(System.in);
+			Studentuser stk = new Studentuser();
+			
+			stk.studentId = sk.nextLine();
+			stk.name = sk.nextLine();
+			
+			stk.displayInfo();
+			
+			sk.close();
+		}
+}
+
+	
