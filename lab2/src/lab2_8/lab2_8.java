53d6797fde9fc62703aa14115c656406d6bb491a
User <User@TG-202308241520>
2025-11-30 00:30:21 +0700

    summitlab

---
+++ lab2/src/lab2_8/lab2_8.java
@@ -0,0 +1,29 @@
+package lab2_8;
+import java.util.Scanner;
+
+class TempConverterlow {
+    public static double CelsiusToFahrenheit(double Celsius) {
+        return (Celsius * 9.0 / 5.0) + 32;
+    }
+    public static double FahrenheitToCelsius(double Fahrenheit) {
+        return (Fahrenheit - 32) * 5.0 / 9.0;
+    }
+}
+
+public class lab2_8 {
+    public static void main(String[] args) {
+        Scanner ScannerOneuser = new Scanner(System.in);
+        String Modetwouser = ScannerOneuser.nextLine();
+        double temperature = ScannerOneuser.nextDouble();
+        double result;
+        if (Modetwouser.equals("C_TO_F")) {
+            result = TempConverterlow.CelsiusToFahrenheit(temperature);
+        } else if (Modetwouser.equals("F_TO_C")) {
+            result = TempConverterlow.FahrenheitToCelsius(temperature);
+        } else {
+            result = temperature; 
+        }
+        System.out.println(result);
+        ScannerOneuser.close();
+    }
+}
