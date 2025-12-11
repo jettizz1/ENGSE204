9c73ba6391491665cb2580a2afa0b4f49dd4e126
User <User@TG-202308241520>
2025-12-11 19:28:38 +0700

    summitlab

--- lab3/src/lab3/lab3_2.java
+++ lab3/src/lab3/lab3_2.java
@@ -18,14 +18,11 @@ class Playeruesr {
 public class lab3_2 {
 	public static void main(String[] args) {
 		Scanner scannerrr = new Scanner(System.in);
-		System.out.print("Enter Score: ");
 		int scoreinput = scannerrr.nextInt();
-		System.out.print("Enter New Score: ");
 		int newscoreinput = scannerrr.nextInt();
 		Playeruesr playerzeroone = new Playeruesr(scoreinput);
 		playerzeroone.setScoreee(newscoreinput);
-		System.out.println("-------Output-------");
-		System.out.println("Score: " + playerzeroone.getScoreee());
+		System.out.println( playerzeroone.getScoreee());
 		scannerrr.close();
 	}
 }
